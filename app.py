0c71ef40f16d8da97003684ffa60519affa60c51
Marie Sengler <senglermarie026@gmail.com>
2025-03-23 12:53:15 +0100

    Change puzzle difficulties

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
 import os
 import divide
+import write_html as wh
+
+col_tf, row_tf = 4, 2
+col_f, row_f = 8, 4
+col_m, row_m = 16, 8
+col_d, row_d = 32, 16
+col_td, row_td = 64, 32
 
 app = Flask(__name__)
 
@@ -11,24 +18,50 @@ def index():
     return render_template('pageaccueil.html')
 
 
-@app.route('/puzzle')
-def puzzle():
-    return render_template('puzzle.html')
+
+@app.route('/puzzle_tf')
+def puzzle_tf():
+    return render_template(f'puzzles_{row_tf}_{col_tf}.html')
+
+@app.route('/puzzle_f')
+def puzzle_f():
+    return render_template(f'puzzles_{row_f}_{col_f}.html')
+
+
+
+
 
 # Route pour exécuter
-@app.route('/run-monde')
-def run_monde():
-    #os.system("python divide.py static/images/Monde.png")
+# Niveaux très facile
+@app.route('/run-monde_tf')
+def run_monde_tf():
     image_path = "static/images/Monde.png"
-    divide.divide_image(image_path, "static/images", 2, 4)
-    return redirect(url_for('puzzle'))
+    divide.divide_image(image_path, "static/images", row_tf, col_tf)
+    wh.generate(row_tf,col_tf)
+    return redirect(url_for('puzzle_tf'))
 
-@app.route('/run-asie')
-def run_asie():
-    #os.system("python divide.py static/images/Asie.png")  
+@app.route('/run-asie_tf')
+def run_asie_tf():
     image_path = "static/images/Asie.png"
-    divide.divide_image(image_path, "static/images", 2, 4)
-    return redirect(url_for('puzzle'))
+    divide.divide_image(image_path, "static/images", row_tf, col_tf)
+    wh.generate(row_tf,col_tf)
+    return redirect(url_for('puzzle_tf'))
+
+
+#Niveau facile
+@app.route('/run-monde_f')
+def run_monde_f():
+    image_path = "static/images/Monde.png"
+    divide.divide_image(image_path, "static/images", row_f, col_f)
+    wh.generate(row_f,col_f)
+    return redirect(url_for('puzzle_f'))
+
+@app.route('/run-asie_f')
+def run_asie_f():
+    image_path = "static/images/Asie.png"
+    divide.divide_image(image_path, "static/images", row_f, col_f)
+    wh.generate(row_f,col_f)
+    return redirect(url_for('puzzle_f'))
 
 
 if __name__ == '__main__':
---
+++ write_html.py
@@ -0,0 +1,138 @@
+import numpy as np
+
+
+
+
+def generate(nb_row = 2, nb_col = 4):
+
+	begin = """
+	<!DOCTYPE HTML>
+	<html>
+	<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="{{ url_for('static', filename='style.css') }}" type="text/css">
+	<title>puzzle</title>
+	"""
+
+	style = f"""
+	<style>
+		.grid-cell {{
+		width: {int(1024/nb_col)}px;
+		height: {int(512/nb_row)}px;
+		border: 1px solid #aaaaaa;
+		position: relative;
+		}}
+
+	</style>
+	"""
+
+	script = """
+	<script>
+		function allowDrop(ev) {
+		ev.preventDefault();
+		}
+
+		function drag(ev) {
+		ev.dataTransfer.setData("text", ev.target.id);
+		}
+
+		function drop(ev) {
+		ev.preventDefault();
+		var data = ev.dataTransfer.getData("text");
+
+		if (ev.target.classList.contains('grid-cell') && ev.target.children.length === 0) {
+			ev.target.appendChild(document.getElementById(data));
+		} 
+		}
+	</script>
+	</head>
+	"""
+
+	nav = """
+	<body>
+		<header>Puzzle du monde </header>
+
+		<nav class="menu">
+			<ul class="menu-list">
+				<li class="menu-item">
+					<a href="/">Accueil</a>
+				</li>
+				<li class="menu-item">
+					<a href="#">Choix Puzzle</a>
+					<ul class="submenu">
+						<li><a href="/run-monde">Monde</a></li>
+						<li><a href="#">Etat-Unis</a></li>
+						<li><a href="#">Europe</a></li>
+						<li><a href="/run-asie">Asie</a></li>
+						<li><a href="#">Importer</a></li>
+					</ul>
+				</li>
+				<li class="menu-item">
+					<a href="#">Difficultés</a>
+					<ul class="submenu">
+						<li><a href="#">Très facile</a></li>
+						<li><a href="#">Facile</a></li>
+						<li><a href="#">Moyen</a></li>
+						<li><a href="#">Difficile</a></li>
+						<li><a href="#">Très difficile</a></li>
+					</ul>
+				</li>
+				<li class="menu-item">
+					<a href="#">À propos</a>
+				</li>
+			</ul>
+		</nav>
+	"""
+
+	map_cell = """
+		<div class="carte">
+			<table border="0" cellspacing="0" cellpadding="0">
+	"""
+
+	idx_id = 0
+	for i in range(nb_row):
+		map_cell = map_cell + "        <tr>\n"
+		for j in range(nb_col):
+			map_cell = map_cell + f"""            <td><div id="cell{idx_id}" class="grid-cell" ondrop="drop(event)" ondragover="allowDrop(event)"></div></td>\n"""
+			idx_id += 1
+		map_cell = map_cell + "        </tr>\n"
+
+	map_cell = map_cell + "        </table>\n    </div>\n"
+
+
+	morceaux = """
+		<div class="morceaux">
+			Placer les morceaux sur la carte:
+			<table border="0" cellspacing="0" cellpadding="0">
+	"""
+
+	index = np.stack([np.repeat(range(nb_row),nb_col),np.array(list(range(nb_col))*nb_row) ], axis=1)
+	index = np.random.permutation(index)
+
+	idx = 0
+	for i in range(nb_row*nb_col//2):
+		morceaux = morceaux + "        <tr>\n"
+		morceaux = morceaux + f"""            <td colspan="3"><img id="drag{idx}" src="{{{{ url_for('static', filename='images/piece_{index[idx,0]}_{index[idx,1]}.png') }}}}" draggable="true" ondragstart="drag(event)" width="{int(1024/nb_col)}" height="{int(512/nb_row)}"></td>\n"""
+		morceaux = morceaux + f"""            <td colspan="3"><img id="drag{idx+1}" src="{{{{ url_for('static', filename='images/piece_{index[idx+1,0]}_{index[idx+1,1]}.png') }}}}" draggable="true" ondragstart="drag(event)" width="{int(1024/nb_col)}" height="{int(512/nb_row)}"></td>\n"""
+		
+		morceaux = morceaux + "        </tr>\n"
+		idx += 2
+
+
+	morceaux = morceaux + """
+			</table>
+		</div>	
+
+	</body>
+	</html>
+	"""
+
+	with open(f"templates/puzzles_{nb_row}_{nb_col}.html", 'w') as f:
+		f.write(begin)
+		f.write(style)
+		f.write(script)
+		f.write(nav)
+		f.write(map_cell)
+		f.write(morceaux)
